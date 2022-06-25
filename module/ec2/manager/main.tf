7aaf21d125fb03d5309ee08009fd14229d27cfde
asemin08 <semin.allan@gmail.com>
2022-06-26 01:25:50 +0200

    Ajout ec2

--- module/ec2/manager/main.tf
+++ module/ec2/manager/main.tf
@@ -12,20 +12,13 @@ resource "aws_instance" "enable-ec2" {
   key_name = "${var.public_ssh_key}"
   private_ip = "${var.ip_ec2}"
 
-  provisioner "remote-exec" {
+  provisioner "local-exec" {
 
     inline = [
-      "sudo yum install -y docker",
-      "sudo systemctl start docker",
+      "echo ${self.public_ip}",
     ]
   }
 
-  connection {
-    host = "${self.public_ip}"
-    type = "ssh"
-    user = "${var.ec2_user}"
-    private_key = "${file("${var.private_ssh_key}")}"
-  }
 
   tags = {
     Name = "${var.name}"
