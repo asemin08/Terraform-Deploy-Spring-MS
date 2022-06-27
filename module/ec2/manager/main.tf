65eadc77852caf2288b5b93d7423b7b917df6d93
asemin08 <semin.allan@gmail.com>
2022-06-27 03:25:01 +0200

    Ajout ec2

--- module/ec2/manager/main.tf
+++ module/ec2/manager/main.tf
@@ -30,6 +30,7 @@ resource "aws_instance" "enable-ec2" {
 
   provisioner "remote-exec" {
     inline = [
+
       "sudo yum update -y",
       "sudo yum install -y git",
       "sudo amazon-linux-extras install -y docker ansible2",
@@ -41,7 +42,8 @@ resource "aws_instance" "enable-ec2" {
       "cp projet_key_pair Terraform-Deploy-Spring-MS",
       "chmod 755 Terraform-Deploy-Spring-MS",
       "cd Terraform-Deploy-Spring-MS",
-      "ansible-playbook -i hosts.yaml playgroud.yaml --private-key=projet_key_pair"
+      "ansible-playbook -i hosts.yaml playgroud.yaml --private-key=projet_key_pair",
+      "ansible-playbook -i hosts.yaml deploy-config.yaml --private-key=projet_key_pair"
     ]
     connection {
       type        = "ssh"
